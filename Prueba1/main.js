45138b051ac14a5962ad09cb3526529c5e2780fd
oscar-abud <oscarpalma.vpr2@gmail.com>
2025-09-15 17:58:06 -0300

    fix: cambios en el formato

--- Prueba1/main.js
+++ Prueba1/main.js
@@ -1,47 +1,52 @@
 window.onload = () => {
+  const $ = (val) => document.querySelector(val);
 
-    const $ = val => document.querySelector(val)
-    
-    const textDiio = $('#outDiio')
-    const textDate = $('#outDate')
-    const textGenre = $('#outGenre')
-    const textRace = $('#outRace')
-    const textLoca = $('#outLoca')
-    const form = $("#form")
-    
-    form.addEventListener('submit', e => {
-        e.preventDefault()
+  const textDiio = $("#outDiio");
+  const textDate = $("#outDate");
+  const textGenre = $("#outGenre");
+  const textRace = $("#outRace");
+  const textLoca = $("#outLoca");
+  const form = $("#form");
 
-        // Obtenemos los valores de los campos sin espacios en los text
-        const diio = Number($('#diio').value.trim())
-        const date = $('#date').value
-        const genre = $('#genre').value
-        const race = $('#race').value.trim()
-        const location = $('#location').value.trim()
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-        textDiio.textContent = '';
-        textDate.textContent = '';
-        textGenre.textContent = '';
-        textRace.textContent = '';
-        textLoca.textContent = '';
-        // Variable aux
-        let state = false
-        // Variable para verificar el genero
-        const validGenre = ['m', 'h']
+    // Obtenemos los valores de los campos sin espacios en los text
+    const diio = Number($("#diio").value.trim());
+    const date = $("#date").value;
+    const genre = $("#genre").value;
+    const race = $("#race").value.trim();
+    const location = $("#location").value.trim();
 
-        textDiio.textContent = diio && !Number.isNaN(diio) ? '' : (Number.isNaN(diio) ? 'Ingrese solo números! ⚠️' : 'Ingrese el DIIO! ⚠️')
+    textDiio.textContent = "";
+    textDate.textContent = "";
+    textGenre.textContent = "";
+    textRace.textContent = "";
+    textLoca.textContent = "";
+    // Variable aux
+    let state = false;
+    // Variable para verificar el genero
+    const validGenre = ["m", "h"];
 
-        if(!date) textDate.textContent = "Ingrese una fecha! ⚠️"
-        if(!validGenre.includes(genre)) textGenre.textContent = "Ingrese el género ⚠️"
-        if(!race) textRace.textContent = "Ingrese la raza!⚠️"
-        if(!location) textLoca.textContent = "Ingrese una ubicación!⚠️"
+    textDiio.textContent =
+      diio && !Number.isNaN(diio)
+        ? ""
+        : Number.isNaN(diio)
+        ? "Ingrese solo números! ⚠️"
+        : "Ingrese el DIIO! ⚠️";
 
-        state = diio && date && validGenre.includes(genre) && race && location
+    if (!date) textDate.textContent = "Ingrese una fecha! ⚠️";
+    if (!validGenre.includes(genre))
+      textGenre.textContent = "Ingrese el género ⚠️";
+    if (!race) textRace.textContent = "Ingrese la raza!⚠️";
+    if (!location) textLoca.textContent = "Ingrese una ubicación!⚠️";
 
-        if(!state) return
-        
-        alert('Vaca registrada con exito!')
-        form.reset()
+    state = diio && date && validGenre.includes(genre) && race && location;
 
-    })
-}
+    if (!state) return;
+
+    alert("Vaca registrada con exito!");
+    form.reset();
+    $("#diio").focus();
+  });
+};

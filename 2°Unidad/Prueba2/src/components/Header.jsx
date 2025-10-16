6c3f9a9fba36374f62b743c39f5aa38f7e304f9d
oscar-abud <oscarpalma.vpr2@gmail.com>
2025-10-16 15:58:31 -0300

    feat: header

--- 2°Unidad/Prueba2/src/components/Header.jsx
+++ 2°Unidad/Prueba2/src/components/Header.jsx
@@ -3,7 +3,7 @@ import logo from "../assets/logo-header.png";
 import Navbar from "./Navbar";
 function Header() {
   return (
-    <div className="bg-light">
+    <div className="bg-light border-bottom">
       <header className="container-sm d-flex justify-content-between align-items-center p-4">
         <img src={logo} alt="logo" className="w-32" />
         <Navbar />

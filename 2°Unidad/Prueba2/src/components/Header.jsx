5ebf06152ee2d4fca66198158126da9e0d1984f9
oscar-abud <oscarpalma.vpr2@gmail.com>
2025-10-16 15:51:04 -0300

    feat: header

--- 2°Unidad/Prueba2/src/components/Header.jsx
+++ 2°Unidad/Prueba2/src/components/Header.jsx
@@ -3,10 +3,12 @@ import logo from "../assets/logo-header.png";
 import Navbar from "./Navbar";
 function Header() {
   return (
-    <header className="flex justify-content-between">
-      <img src={logo} alt="logo" />
-      <Navbar />
-    </header>
+    <div className="bg-light">
+      <header className="container-sm d-flex justify-content-between align-items-center p-4">
+        <img src={logo} alt="logo" className="w-32" />
+        <Navbar />
+      </header>
+    </div>
   );
 }
 
